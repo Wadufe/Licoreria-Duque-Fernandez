2633755f505843e3ffe93acbf4b563d1b08b4404
walter duque <walterduque13@gmail.com>
2022-07-25 22:47:49 -0500

    implentacion de boostrap  y la libreria swiper en el proyecto

---
+++ swiper_index.js
@@ -0,0 +1,43 @@
+var swiper1 = new Swiper(".swiper-container-1", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4700,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper2 = new Swiper(".swiper-container-2", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  rewind: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  },
+});
